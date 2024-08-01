9a632614ed5db21131ef6a81b6b844011a23ede8
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2024-08-01 23:01:44 +0530

    done

--- dsa assignment/sem2/assignment2/online stock span.py
+++ dsa assignment/sem2/assignment2/online stock span.py
@@ -1,1 +1,22 @@
 # leetcode id-deepali0402
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int sp=1;
+        while(!st.empty() && st.top().first<=price){
+            sp+=st.top().second;
+            st.pop();
+        }
+        st.push({price,sp}) ;
+        return sp;
+        
+    }
+
+private:
+    stack <pair<int,int>> st;
+};
+
